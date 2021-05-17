214f64fb1e3fc08338ffaeebd3de6cff39df8bbc
Mateusz <Mateusz@DESKTOP-0TA90MJ.local>
2021-05-17 09:45:29 +0200

    d

--- PR-KINO/src/application/Main.java
+++ PR-KINO/src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			System.out.println("GIT test");
-			System.out.println("GIT test22222");
+			System.out.println("GIT test2222dd2");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
