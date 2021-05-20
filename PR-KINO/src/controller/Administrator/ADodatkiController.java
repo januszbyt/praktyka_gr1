ec1963c33b2d97dd4af85bd053b9f39990567401
BacharaOleh <olegbacara@gmail.com>
2021-05-20 11:57:50 +0200

    o_O

--- PR-KINO/src/controller/Administrator/ADodatkiController.java
+++ PR-KINO/src/controller/Administrator/ADodatkiController.java
@@ -1,10 +1,41 @@
 package controller.Administrator;
 
 import controller.MainController;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ADodatkiController {
+
     private MainController main;
     public void init(MainController main){
         this.main = main;
+    }
+    @FXML
+    public TextField t1;
+    @FXML public TextField t2;
+    @FXML public TextField t3;
+    @FXML public TextField t4;
+
+    public void wpiszDane() {
+        ADodatkiController af = new ADodatkiController();
+        System.out.println("INFORMACJE O DODATKU");
+        System.out.println("Nazwa: " + t1.getText());
+        System.out.println("Typ: " + t2.getText());
+        System.out.println("Cena: " + t3.getText());
+        System.out.println("Ilość: " + t4.getText());
+        {
+            ADodatkiController af2= new ADodatkiController();
+
+        }
+
+    }
+
+    public void wyczyscPola()
+    {
+        t1.clear();
+        t2.clear();
+        t3.clear();
+        t4.clear();
+
     }
 }
