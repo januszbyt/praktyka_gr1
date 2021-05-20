369c5e073b40b81ce0588e73a13d21446669e2d7
AlfaGruisPL <alfagruis@gmail.com>
2021-05-20 10:36:51 +0200

    (☞ﾟヮﾟ)☞

--- PR-KINO/src/controller/MainController.java
+++ PR-KINO/src/controller/MainController.java
@@ -6,8 +6,8 @@ import java.sql.*;
 
 public class MainController {
 
-	static Connection con;
-	static Statement stmt;
+	public static Connection con;
+	public static Statement stmt;
 	@FXML public Pane body;
 	@FXML public ComboBox WyborFilmu;
 	@FXML public  ViewController ViewController = new ViewController();;
@@ -22,10 +22,10 @@ public class MainController {
 	public void Wybierz(){
 		ViewController.changeBody(WyborFilmu.getValue()+"");
 	}
-	public void PolaczZBazaDanych(){
+	private void PolaczZBazaDanych(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://sql11.freemysqlhosting.net","sql11412360\n","MLKI6a5Yh3");
+			con=DriverManager.getConnection("jdbc:mysql://sql11.freemysqlhosting.net/sql11412360","sql11412360","MLKI6a5Yh3");
 			stmt=con.createStatement();
 			ResultSet rs=stmt.executeQuery("select * from filmy");
 			while(rs.next())
