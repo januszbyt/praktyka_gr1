fb0e5ba06dc3b2fb16331429ad5e2b46f4246839
Mateusz <mateusz7241@gmail.com>
2021-05-24 09:59:58 +0200

    a

--- PR-KINO/src/controller/Administrator/AUżytkownicyController.java
+++ PR-KINO/src/controller/Administrator/AUżytkownicyController.java
@@ -1,8 +1,23 @@
 package controller.Administrator;
 
 import controller.MainController;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AUżytkownicyController {
+
+    @FXML public TextField t1;
+    @FXML public TextField t2;
+    @FXML public TextField t3;
+    @FXML public TextField t4;
+    @FXML public TextField t5;
+    @FXML public TextField t6;
+    @FXML public TextField tid;
+
+
+
+
     private MainController main;
     public void init(MainController main){
         this.main = main;
