c4e1eef0b1e0dcc6d25ccea7426505b0d7942b5c
Kornelia <korneliamushak@gmail.com>
2021-05-25 09:25:35 +0200

    logowanie

--- PR-KINO/src/controller/Logowanie/WyborOkienekUzytController.java
+++ PR-KINO/src/controller/Logowanie/WyborOkienekUzytController.java
@@ -7,4 +7,11 @@ public class WyborOkienekUzytController {
     public void init(MainController main){
         this.main = main;
     }
+    public void przejscie_do_recenzji(){
+main.ViewController.changeBody("URecenzja");
+    }
+    public void przejscie_do_zakupu(){
+        main.ViewController.changeBody("UWybor_filmu");
+    }
+
 }
