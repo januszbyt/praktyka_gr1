11f9170f64bcdac66509c925fae4421a031e10b6
BacharaOleh <olegbacara@gmail.com>
2021-05-26 10:32:23 +0200

    Modykikowanie ADodatki

--- PR-KINO/src/object/Admin_dodatki.java
+++ PR-KINO/src/object/Admin_dodatki.java
@@ -1,33 +1,31 @@
 package object;
 
 public class Admin_dodatki {
+    private Integer id;
     private String nazwa;
     private String typ;
-    private Integer cena;
+    private Double cena;
     private Integer ilosc;
-    private Integer id;
-    public Admin_dodatki( Integer id,String nazwa, String typ, Integer cena,Integer ilosc) {
-       this.id = id;
+
+    public Admin_dodatki(Integer id,String nazwa, String typ, Double cena,Integer ilosc) {
+        this.id=id;
         this.nazwa = nazwa;
         this.typ = typ;
         this.cena = cena;
         this.ilosc = ilosc;
     }
-
-    public String getnazwa(){return nazwa;}
-    public void setnazwa(String nz){this.nazwa=nz;}
-    public String gettyp()
-
-    {
-        return typ;
-    }
-    public void settyp(String tp) { this.typ=tp; }
-    public int getcena(){ return cena; }
-    public void setCena(Integer ca)  {  this.cena=ca;   }
-    public int getilosc(){ return ilosc; }
-    public void setilosc(Integer ic)    {  this.ilosc=ic;}
-    public void setId(Integer id)    {  this.id=id;   }
+    public String getNazwa(){return nazwa;}
+    public void setNazwa(String nz){this.nazwa=nz;}
+    public String getTyp()  { return typ; }
+    public void setTyp(String tp) { this.typ=tp; }
+    public Double getCena(){ return cena; }
+    public void setCena(Double ca)  {  this.cena=ca;   }
+    public Integer getIlosc(){ return ilosc; }
+    public void setIlosc(Integer ic)    {  this.ilosc=ic;}
     public Integer getId(){ return id;}
+    public void setId(Integer id1){ this.id=id1;}
+
+
 
 
 }
