acc27981a2e556d6b922dc236fd9b88057043cc1
NeKoRoM <m.pnikut@gmail.com>
2021-05-27 00:34:22 +0300

    okienko wyboru filmu użytkownik(nie pracują przyciski)

--- PR-KINO/src/controller/Użytkownik/UWybor_filmuController.java
+++ PR-KINO/src/controller/Użytkownik/UWybor_filmuController.java
@@ -21,36 +21,62 @@ public class UWybor_filmuController<topBtn> {
         this.initialize();
     }
     public void initialize() {
-        int[] idF1= new int[47];
-        String[] nazwaF= new String[47];
-        String[] urlF= new String[47];
-        int i =0;
+        int[] idF = new int[47];
+        String[] nazwaF = new String[47];
+        String[] urlF = new String[47];
+        int i = 0;
+        int id;
 
-            try {
-                ResultSet rs = this.main.stmt.executeQuery("SELECT *,filmy.id as aa, (select avg (ocena) from recencje where recencje.id_filmu= aa) as srednia from filmy order by srednia desc");
-                while (rs.next()) {
-                    idF1[i]= rs.getInt(1);
-                    nazwaF[i]= rs.getString(2);
-                    urlF[i]= rs.getString(10);
-                    System.out.println(idF1[i]+ nazwaF[i]+urlF[i]);
-                }
-            }catch(Exception e){
-                System.out.println(e);
+        try {
+            ResultSet rs = this.main.stmt.executeQuery("SELECT *,filmy.id as aa, (select avg (ocena) from recencje where recencje.id_filmu= aa) as srednia from filmy order by srednia desc");
+            while (rs.next()) {
+                idF[i] = rs.getInt(1);
+                nazwaF[i] = rs.getString(2);
+                urlF[i] = rs.getString(10);
+                System.out.println(idF[i] + nazwaF[i] + urlF[i]);
+                i++;
             }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-            for(int j=0,k=0;j<6;j++)
-                for(int l=0;l<8;l++,k++)
-                {
-                    try {
+        //System.out.println(urlF[0]);
+
+        for (int j = 0, k = 0; j < 6; j++)
+            for (int l = 0; l < 8; l++, k++) {
+                try {
                     System.out.println(urlF[k]);
-                   String url =urlF[k];
-                    Image image = new Image(url, 100, 100, false, true);
-                    button_grid.add(new ImageView(image),j,l);
-                    Button btn = new Button(nazwaF[k]);
-                    button_grid.add(btn, j,l);
-                    }catch (Exception e){}
+                    if (urlF[k]!=null){
+                    Image image = new Image(urlF[k], 100, 100, false, true);
+                    button_grid.add(new ImageView(image), l, j);
+                    }
+                    else if(nazwaF[k]!=null){
+                        String url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Brak_obrazka.svg/1024px-Brak_obrazka.svg.png";
+                        Image image = new Image(url, 100, 100, false, true);
+                        button_grid.add(new ImageView(image), l, j);
 
+                    }
+                    if (nazwaF[k]!=null) {
+                        Button btn = new Button(nazwaF[k]);
+                        /*btn.setOnAction(new EventHandler<ActionEvent>() {
+
+
+                                            public void handle(ActionEvent event) {
+                                                id=idF[k];
+
+
+                                            }
+                                        }*/
+                        button_grid.add(btn, l, j);
+                    }
+
+                } catch (Exception e) {
                 }
+            }
+        //return id;
+    }
+
+
         /*String url = "http://83437-254797-raikfcquaxqncofqfm.stackpathdns.com/17167-home_default/obrazek-obrazki-19paz.jpg";
         Image image = new Image(url, 100, 100, false, true);
         button_grid.add(new ImageView(image),0,0);
@@ -63,7 +89,7 @@ public class UWybor_filmuController<topBtn> {
         button_grid.add(btn, indexX,indexY);*/
 
 
-    }
+
    /* void addButton(){
         int index = 2;
         Button sound_button = new Button("Button_" + index);
