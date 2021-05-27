c69e621056db23d00b741332d0e0f0e7cf597ba2
AlfaGruisPL <alfagruis@gmail.com>
2021-05-27 09:32:03 +0200

    Dodanie pustego panelu wyboru seansu i zarządzanie seansami z poziomu administratra

---
+++ PR-KINO/src/controller/Administrator/ASeansuController.java
@@ -0,0 +1,11 @@
+package controller.Administrator;
+
+import controller.MainController;
+
+public class ASeansuController {
+    private MainController main;
+
+    public void init(MainController main) {
+        this.main = main;
+    }
+}
---
+++ PR-KINO/src/controller/Użytkownik/UWybur_seansuController.java
@@ -0,0 +1,11 @@
+package controller.Użytkownik;
+
+import controller.MainController;
+
+public class UWybur_seansuController {
+    private MainController main;
+
+    public void init(MainController main) {
+        this.main = main;
+    }
+}
