733957bdb5c1148a45a4754965de60c46932a018
Mateusz <mateusz7241@gmail.com>
2021-05-27 11:52:07 +0200

    przyciski powrót w Afilmy Adodatki i Auzytkownicy wracaja do WyborOknaAdministrator

--- PR-KINO/src/controller/Administrator/ASeansuController.java
+++ PR-KINO/src/controller/Administrator/ASeansuController.java
@@ -1,6 +1,7 @@
 package controller.Administrator;
 
 import controller.MainController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -14,7 +15,10 @@ public class ASeansuController {
         this.main = main;
     }
 
+    public void powrotButton() {
+        main.ViewController.changeBody("WyborOkienekAdmin");
     }
+}
 
 
 
