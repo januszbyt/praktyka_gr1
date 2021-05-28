c72f122250ed69209ea4624309081c929fffc0aa
Mateusz <mateusz7241@gmail.com>
2021-05-28 14:17:24 +0200

    Mała aktualizacja wyglądu okna Aseans, pierwsze funkcje AseansController (do poprawy)

--- PR-KINO/src/controller/Administrator/ASeansuController.java
+++ PR-KINO/src/controller/Administrator/ASeansuController.java
@@ -1,12 +1,8 @@
 package controller.Administrator;
 
 import controller.MainController;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-
-import java.sql.ResultSet;
+import javafx.scene.control.TextField;
 
 public class ASeansuController {
     private MainController main;
@@ -15,8 +11,38 @@ public class ASeansuController {
         this.main = main;
     }
 
+    @FXML public TextField tid;
+    @FXML public TextField t1;
+    @FXML public TextField t2;
+    @FXML public TextField t3;
+    @FXML public TextField t4;
+    @FXML public TextField t5;
+
+
+
     public void powrotButton() {
         main.ViewController.changeBody("WyborOkienekAdmin");
+    }
+
+    public void dodajSeans() {
+        try {
+
+            Integer ID = Integer.valueOf(tid.getText());
+            String Date = t1.getText();
+            String Wersja = t2.getText();
+            String Typ = (t3.getText());
+            Integer IDSali = Integer.valueOf(t4.getText());
+            Integer IDFilm = Integer.valueOf(t5.getText());
+
+
+
+            String query = "INSERT INTO `filmy` (`id`, `start`, `wersja`, `id_sali`, `id_filmu`) VALUES ('"+ID+"','"+Date+"','"+Wersja+"','"+Typ+"','"+IDSali +"','"+IDFilm+"')";
+            this.main.stmt.execute(query);
+            System.out.println("Pomyślnie dodano seans o ID: "+ ID);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
 
